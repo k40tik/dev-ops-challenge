88e3f794ea80689c88be341c34a5718b92f8f70a
kaotik <jkilonzi301@gmail.com>
2026-09-14 11:35:43 +0300

    corrected port issue

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000;
 app.get('/hello',(req,res) =>{
 res.send("HELLO, welcome to HehA");
 })
-app.listen(port,()=>{console.log('server running on port ${port}')})
+app.listen(port,()=>{console.log(`server running on port ${port}`)})
