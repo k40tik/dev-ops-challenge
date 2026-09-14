58297d4b46071994d3435148c288e17241276118
kaotik <jkilonzi301@gmail.com>
2026-09-14 11:24:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require ('express')
+const app = express();
+const port = 3000;
+app.get('/hello',(req,res) =>{
+res.send("HELLO, welcome to HehA");
+})
+app.listen(port,()=>{console.log('server running on port ${port}')})
